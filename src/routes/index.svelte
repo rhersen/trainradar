f4e12093c0e87a5e97fd092793fbb8820a0a1bc4
Rudolf Hersén <rudolf.hersen@schibsted.com>
2022-04-08 11:18:08 +0200

    show station code on mouseover

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,26 @@
 <script>
   export let locations;
+
+  let station;
+
+  function handleMouseOver(signature) {
+    return () => {
+      station = signature;
+    };
+  }
 </script>
 
 <svg class="root" viewBox="0 0 1 1">
-  {#each locations as { east, north }}
-    <circle class="dot" cx={east - 17.5} cy={59.8 - north} />
+  {#each locations as { signature, east, north }}
+    <circle
+      class="dot"
+      on:mouseover={handleMouseOver(signature)}
+      cx={east - 17.5}
+      cy={59.8 - north}
+    />
   {/each}
 </svg>
+<h1>{station}</h1>
 
 <style>
   .root {
