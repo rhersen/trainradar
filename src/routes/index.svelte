3f5695dd2d35a4154f5987a6d26b558ab1ba2f74
Rudolf Hersén <rudolf.hersen@schibsted.com>
2022-04-08 10:46:48 +0200

    dark bg

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,8 +2,19 @@
   export let locations;
 </script>
 
-<svg width="100%" viewBox="0 0 16 16">
+<svg class="root" viewBox="0 0 16 16">
   {#each locations as { east, north }}
-    <circle r=".04" cx={east - 10} cy={70 - north} />
+    <circle class="dot" cx={east - 10} cy={70 - north} />
   {/each}
 </svg>
+
+<style>
+  .root {
+    background: black;
+  }
+
+  .dot {
+    r: 0.04;
+    fill: lightgray;
+  }
+</style>
