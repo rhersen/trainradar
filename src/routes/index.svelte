237a4597fa988f504682a62b6d22f725284dd307
Rudolf Hersén <rudolf.hersen@schibsted.com>
2022-04-08 13:53:21 +0200

    show activity text

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,34 +2,54 @@
   export let locations;
   export let arrivals;
 
-  let station;
+  let hoverText;
 
-  function handleMouseOver(name) {
+  function handleMouseOver(code, name) {
     return () => {
-      station = name;
+      if (arrivals[code]?.length === 1) {
+        const [
+          {
+            TimeAtLocationWithSeconds,
+            AdvertisedTimeAtLocation,
+            AdvertisedTrainIdent,
+          },
+        ] = arrivals[code];
+        const actual = Date.parse(TimeAtLocationWithSeconds);
+        const advertised = Date.parse(AdvertisedTimeAtLocation);
+        hoverText = `Tåg ${AdvertisedTrainIdent} är ${
+          (actual - advertised) * 1e-3
+        } sekunder sent i ${name}`;
+      } else {
+        hoverText = `${arrivals[code]?.length} tåg i ${name}`;
+      }
     };
   }
 </script>
 
-<svg class="root" viewBox="0 0 1 1">
+<svg class="root" viewBox="0 0 16 16">
   {#each locations as { code, name, east, north }}
     <circle
-      class="dot"
-      on:mouseover={handleMouseOver(name)}
-      cx={east - 17.5}
-      cy={59.8 - north}
-      fill={arrivals[code] ? 'cyan' : 'lightgray'}
+      class={arrivals[code] ? 'active' : 'passive'}
+      on:mouseover={handleMouseOver(code, name)}
+      cx={east - 10}
+      cy={70 - north}
     />
   {/each}
 </svg>
-<h1>{station}</h1>
+<h1>{hoverText}</h1>
 
 <style>
   .root {
     background: black;
   }
 
-  .dot {
-    r: 0.005;
+  .active {
+    r: 0.05;
+    fill: lightgreen;
+  }
+
+  .passive {
+    r: 0.02;
+    fill: darkgray;
   }
 </style>
